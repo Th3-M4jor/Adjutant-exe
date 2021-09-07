37fe3ec5540dae7ad1d153f602c204a990f5a9dc
The Major <spartan364@gmail.com>
2021-09-07 13:00:09 -0700

    update dialyzer

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ElixirBot.MixProject do
       {:dotenv, "~> 3.1"},
       {:httpoison, "~> 1.7"},
       {:poison, "~> 3.0"},
-      {:dialyxir, "~> 0.4", only: [:dev]}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
