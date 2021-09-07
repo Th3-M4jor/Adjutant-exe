e35257237a3ccd72ca3412319b8af45bbae87559
The Major <spartan364@gmail.com>
2021-09-07 12:46:50 -0700

    add phb links

--- lib/commands/bnb/phb.ex
+++ lib/commands/bnb/phb.ex
@@ -6,27 +6,22 @@ defmodule BnBBot.Commands.PHB do
   @behaviour BnBBot.SlashCmdFn
 
   def call_slash(%Nostrum.Struct.Interaction{} = inter) do
-    phb_url = Application.fetch_env!(:elixir_bot, :phb)
+    link_buttons = Application.fetch_env!(:elixir_bot, :phb_links)
+      |> Enum.chunk_every(5)
+      |> Enum.map(fn links ->
+        %{
+          type: 1,
+          components: links
+        }
+      end)
 
     {:ok} = Api.create_interaction_response(
       inter,
       %{
         type: 4,
         data: %{
-          content: "B&B Players Handbook Links:",
-          components: [
-            %{
-              type: 1,
-              components: [
-                %{
-                  type: 2,
-                  style: 5,
-                  label: "B&B PHB",
-                  url: phb_url
-                }
-              ]
-            }
-          ]
+          content: "B&B Links:",
+          components: link_buttons
         }
       }
     )
@@ -36,8 +31,8 @@ defmodule BnBBot.Commands.PHB do
   def get_create_map() do
     %{
       type: 1,
-      name: "phb",
-      description: "Get a link to the PHB"
+      name: "links",
+      description: "Get a link to the PHB and Manager"
     }
   end
 end
