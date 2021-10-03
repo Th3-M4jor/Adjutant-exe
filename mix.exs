0643adbc5abc04c4db8b2f9ef8f679b1eaebb956
The Major <spartan364@gmail.com>
2021-10-03 22:18:28 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule ElixirBot.MixProject do
       deps: deps(),
       releases: [
         bnb_bot: [
-          version: "0.2.0",
+          version: "0.2.1",
           applications: [
             elixir_bot: :permanent
           ],
