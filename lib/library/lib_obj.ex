0f1245da9cbb7b66e03a6ea965284dcf3b335d15
The Major <spartan364@gmail.com>
2022-02-08 21:15:40 +0000

    remove unused specs

--- lib/library/lib_obj.ex
+++ lib/library/lib_obj.ex
@@ -204,30 +204,6 @@ defmodule BnBBot.Library.Shared do
 end
 
 defprotocol BnBBot.Library.LibObj do
-  @type button_style :: 1..4
-
-  @type partial_emoji :: %{
-          required(:name) => String.t(),
-          optional(:id) => Nostrum.Snowflake.external_snowflake(),
-          optional(:animated) => boolean()
-        }
-
-  @type button :: %{
-          required(:type) => 2,
-          required(:style) => button_style,
-          required(:label) => String.t(),
-          required(:custom_id) => String.t(),
-          optional(:emoji) => partial_emoji(),
-          optional(:disabled) => boolean()
-        }
-
-  @type link_button :: %{
-          required(:type) => 2,
-          required(:style) => 5,
-          required(:label) => String.t(),
-          required(:url) => String.t(),
-          optional(:emoji) => partial_emoji()
-        }
 
   @doc """
   Return the type of the libobj.
