89cf9fda06bed1a051c695fa1508e7f6dce3c2d5
The Major <spartan364@gmail.com>
2022-05-15 03:50:49 +0000

    update structure

--- priv/postgres/structure.sql
+++ priv/postgres/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
--- Dumped by pg_dump version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+-- Dumped from database version 12.10 (Ubuntu 12.10-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.10 (Ubuntu 12.10-0ubuntu0.20.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,42 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: oban_jobs; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA oban_jobs;
+
+
+--
+-- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching based on trigrams';
+
+
+--
+-- Name: oban_job_state; Type: TYPE; Schema: oban_jobs; Owner: -
+--
+
+CREATE TYPE oban_jobs.oban_job_state AS ENUM (
+    'available',
+    'scheduled',
+    'executing',
+    'retryable',
+    'completed',
+    'discarded',
+    'cancelled'
+);
+
 
 --
 -- Name: Dice; Type: TYPE; Schema: public; Owner: -
@@ -154,9 +190,101 @@ CREATE TYPE public."Skill" AS ENUM (
 );
 
 
+--
+-- Name: oban_jobs_notify(); Type: FUNCTION; Schema: oban_jobs; Owner: -
+--
+
+CREATE FUNCTION oban_jobs.oban_jobs_notify() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  channel text;
+  notice json;
+BEGIN
+  IF NEW.state = 'available' THEN
+    channel = 'oban_jobs.oban_insert';
+    notice = json_build_object('queue', NEW.queue);
+
+    PERFORM pg_notify(channel, notice::text);
+  END IF;
+
+  RETURN NULL;
+END;
+$$;
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: oban_jobs; Type: TABLE; Schema: oban_jobs; Owner: -
+--
+
+CREATE TABLE oban_jobs.oban_jobs (
+    id bigint NOT NULL,
+    state oban_jobs.oban_job_state DEFAULT 'available'::oban_jobs.oban_job_state NOT NULL,
+    queue text DEFAULT 'default'::text NOT NULL,
+    worker text NOT NULL,
+    args jsonb DEFAULT '{}'::jsonb NOT NULL,
+    errors jsonb[] DEFAULT ARRAY[]::jsonb[] NOT NULL,
+    attempt integer DEFAULT 0 NOT NULL,
+    max_attempts integer DEFAULT 20 NOT NULL,
+    inserted_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    scheduled_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    attempted_at timestamp without time zone,
+    completed_at timestamp without time zone,
+    attempted_by text[],
+    discarded_at timestamp without time zone,
+    priority integer DEFAULT 0 NOT NULL,
+    tags character varying(255)[] DEFAULT ARRAY[]::character varying[],
+    meta jsonb DEFAULT '{}'::jsonb,
+    cancelled_at timestamp without time zone,
+    CONSTRAINT attempt_range CHECK (((attempt >= 0) AND (attempt <= max_attempts))),
+    CONSTRAINT positive_max_attempts CHECK ((max_attempts > 0)),
+    CONSTRAINT priority_range CHECK (((priority >= 0) AND (priority <= 3))),
+    CONSTRAINT queue_length CHECK (((char_length(queue) > 0) AND (char_length(queue) < 128))),
+    CONSTRAINT worker_length CHECK (((char_length(worker) > 0) AND (char_length(worker) < 128)))
+);
+
+
+--
+-- Name: TABLE oban_jobs; Type: COMMENT; Schema: oban_jobs; Owner: -
+--
+
+COMMENT ON TABLE oban_jobs.oban_jobs IS '11';
+
+
+--
+-- Name: oban_jobs_id_seq; Type: SEQUENCE; Schema: oban_jobs; Owner: -
+--
+
+CREATE SEQUENCE oban_jobs.oban_jobs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: oban_jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: oban_jobs; Owner: -
+--
+
+ALTER SEQUENCE oban_jobs.oban_jobs_id_seq OWNED BY oban_jobs.oban_jobs.id;
+
+
+--
+-- Name: oban_peers; Type: TABLE; Schema: oban_jobs; Owner: -
+--
+
+CREATE UNLOGGED TABLE oban_jobs.oban_peers (
+    name text NOT NULL,
+    node text NOT NULL,
+    started_at timestamp without time zone NOT NULL,
+    expires_at timestamp without time zone NOT NULL
+);
+
 
 --
 -- Name: Battlechip; Type: TABLE; Schema: public; Owner: -
@@ -294,6 +422,13 @@ CREATE TABLE public.schema_migrations (
 
 
 --
+-- Name: oban_jobs id; Type: DEFAULT; Schema: oban_jobs; Owner: -
+--
+
+ALTER TABLE ONLY oban_jobs.oban_jobs ALTER COLUMN id SET DEFAULT nextval('oban_jobs.oban_jobs_id_seq'::regclass);
+
+
+--
 -- Name: Battlechip id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -312,6 +447,22 @@ ALTER TABLE ONLY public."NaviCust" ALTER COLUMN id SET DEFAULT nextval('public."
 --
 
 ALTER TABLE ONLY public."Virus" ALTER COLUMN id SET DEFAULT nextval('public."Virus_id_seq"'::regclass);
+
+
+--
+-- Name: oban_jobs oban_jobs_pkey; Type: CONSTRAINT; Schema: oban_jobs; Owner: -
+--
+
+ALTER TABLE ONLY oban_jobs.oban_jobs
+    ADD CONSTRAINT oban_jobs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: oban_peers oban_peers_pkey; Type: CONSTRAINT; Schema: oban_jobs; Owner: -
+--
+
+ALTER TABLE ONLY oban_jobs.oban_peers
+    ADD CONSTRAINT oban_peers_pkey PRIMARY KEY (name);
 
 
 --
@@ -363,10 +514,59 @@ ALTER TABLE ONLY public.schema_migrations
 
 
 --
+-- Name: oban_jobs_args_index; Type: INDEX; Schema: oban_jobs; Owner: -
+--
+
+CREATE INDEX oban_jobs_args_index ON oban_jobs.oban_jobs USING gin (args);
+
+
+--
+-- Name: oban_jobs_meta_index; Type: INDEX; Schema: oban_jobs; Owner: -
+--
+
+CREATE INDEX oban_jobs_meta_index ON oban_jobs.oban_jobs USING gin (meta);
+
+
+--
+-- Name: oban_jobs_state_queue_priority_scheduled_at_id_index; Type: INDEX; Schema: oban_jobs; Owner: -
+--
+
+CREATE INDEX oban_jobs_state_queue_priority_scheduled_at_id_index ON oban_jobs.oban_jobs USING btree (state, queue, priority, scheduled_at, id);
+
+
+--
 -- Name: Virus_name_index; Type: INDEX; Schema: public; Owner: -
 --
 
 CREATE UNIQUE INDEX "Virus_name_index" ON public."Virus" USING btree (name);
+
+
+--
+-- Name: chip_trgm_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX chip_trgm_idx ON public."Battlechip" USING gin (name public.gin_trgm_ops);
+
+
+--
+-- Name: ncp_trgm_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX ncp_trgm_idx ON public."NaviCust" USING gin (name public.gin_trgm_ops);
+
+
+--
+-- Name: virus_trgm_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX virus_trgm_idx ON public."Virus" USING gin (name public.gin_trgm_ops);
+
+
+--
+-- Name: oban_jobs oban_notify; Type: TRIGGER; Schema: oban_jobs; Owner: -
+--
+
+CREATE TRIGGER oban_notify AFTER INSERT ON oban_jobs.oban_jobs FOR EACH ROW EXECUTE FUNCTION oban_jobs.oban_jobs_notify();
 
 
 --
@@ -380,3 +580,4 @@ INSERT INTO public."schema_migrations" (version) VALUES (20220131190559);
 INSERT INTO public."schema_migrations" (version) VALUES (20220202171429);
 INSERT INTO public."schema_migrations" (version) VALUES (20220203230937);
 INSERT INTO public."schema_migrations" (version) VALUES (20220317033651);
+INSERT INTO public."schema_migrations" (version) VALUES (20220428032204);
