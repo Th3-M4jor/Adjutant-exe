ebc1e53d412897ab2067fc3241d37628ceb9f685
The Major <spartan364@gmail.com>
2022-06-11 01:35:46 +0000

    code deduplication

--- lib/webhook/update.ex
+++ lib/webhook/update.ex
@@ -9,62 +9,7 @@ defmodule BnBBot.Webhook.Update do
 
   @spec should_update?(String.t(), Nostrum.Snowflake.t()) :: boolean()
   def should_update?(msg, user_id) do
-    uuid =
-      System.unique_integer([:positive])
-      # constrain to be between 0 and 0xFF_FF_FF
-      |> Bitwise.band(0xFF_FF_FF)
-
-    buttons = make_yes_no_buttons(uuid)
-
-    dm_channel_id = BnBBot.Util.find_dm_channel_id(user_id)
-
-    msg =
-      Api.create_message!(dm_channel_id, %{
-        content: msg,
-        components: buttons
-      })
-
-    btn_response = BnBBot.ButtonAwait.await_btn_click(uuid, nil)
-
-    case btn_response do
-      {btn_inter, "yes"} ->
-        Task.start(fn ->
-          Api.create_interaction_response!(btn_inter, %{
-            type: 7,
-            data: %{
-              content: "Updating...",
-              components: []
-            }
-          })
-        end)
-
-        true
-
-      {btn_inter, "no"} ->
-        Task.start(fn ->
-          Api.create_interaction_response!(btn_inter, %{
-            type: 7,
-            data: %{
-              content: "Not updating.",
-              components: []
-            }
-          })
-        end)
-
-        false
-
-      nil ->
-        Logger.debug("No response received, assuming false")
-
-        Task.start(fn ->
-          Api.edit_message!(msg, %{
-            content: "Timed out waiting for response, assuming No",
-            components: []
-          })
-        end)
-
-        false
-    end
+    ask_question(msg, user_id, affirmed: "Updating...", negated: "Not updating...")
   rescue
     e ->
       Logger.error(Exception.format(:error, e, __STACKTRACE__))
@@ -73,62 +18,7 @@ defmodule BnBBot.Webhook.Update do
 
   @spec should_announce?(String.t(), Nostrum.Snowflake.t()) :: boolean
   def should_announce?(msg, user_id) do
-    uuid =
-      System.unique_integer([:positive])
-      # constrain to be between 0 and 0xFF_FF_FF
-      |> Bitwise.band(0xFF_FF_FF)
-
-    buttons = make_yes_no_buttons(uuid)
-
-    dm_channel_id = BnBBot.Util.find_dm_channel_id(user_id)
-
-    msg =
-      Api.create_message!(dm_channel_id, %{
-        content: msg,
-        components: buttons
-      })
-
-    btn_response = BnBBot.ButtonAwait.await_btn_click(uuid, nil)
-
-    case btn_response do
-      {btn_inter, "yes"} ->
-        Task.start(fn ->
-          Api.create_interaction_response!(btn_inter, %{
-            type: 7,
-            data: %{
-              content: "Announcing...",
-              components: []
-            }
-          })
-        end)
-
-        true
-
-      {btn_inter, "no"} ->
-        Task.start(fn ->
-          Api.create_interaction_response!(btn_inter, %{
-            type: 7,
-            data: %{
-              content: "Not announcing",
-              components: []
-            }
-          })
-        end)
-
-        false
-
-      nil ->
-        Logger.debug("No response received, assuming false")
-
-        Task.start(fn ->
-          Api.edit_message!(msg, %{
-            content: "Timed out waiting for response, assuming No",
-            components: []
-          })
-        end)
-
-        false
-    end
+    ask_question(msg, user_id, affirmed: "Announcing...", negated: "Not announcing")
   rescue
     e ->
       Logger.error(Exception.format(:error, e, __STACKTRACE__))
@@ -146,6 +36,63 @@ defmodule BnBBot.Webhook.Update do
     dm_channel_id = BnBBot.Util.find_dm_channel_id(user_id)
 
     Api.create_message!(dm_channel_id, msg)
+  end
+
+  defp ask_question(question, user_id, responses) do
+    affirmed = responses[:affirmed] || "`Yes` selected"
+    negated = responses[:negated] || "`No` selected"
+    timeout = responses[:timeout] || "Timed out waiting for response, assuming `No`"
+
+    uuid =
+      System.unique_integer([:positive])
+      # constrain to be between 0 and 0xFF_FF_FF
+      |> Bitwise.band(0xFF_FF_FF)
+
+    buttons = make_yes_no_buttons(uuid)
+
+    dm_channel_id = BnBBot.Util.find_dm_channel_id(user_id)
+
+    msg =
+      Api.create_message!(dm_channel_id, %{
+        content: question,
+        components: buttons
+      })
+
+    btn_response = BnBBot.ButtonAwait.await_btn_click(uuid, nil)
+
+    case btn_response do
+      {btn_inter, "yes"} ->
+        edit_btn_response(btn_inter, affirmed)
+        true
+
+      {btn_inter, "no"} ->
+        edit_btn_response(btn_inter, negated)
+        false
+
+      nil ->
+        Logger.debug("No response received, assuming false")
+
+        Task.start(fn ->
+          Api.edit_message!(msg, %{
+            content: timeout,
+            components: []
+          })
+        end)
+
+        false
+    end
+  end
+
+  defp edit_btn_response(inter, content) do
+    Task.start(fn ->
+      Api.create_interaction_response!(inter, %{
+        type: 7,
+        data: %{
+          content: content,
+          components: []
+        }
+      })
+    end)
   end
 
   defp make_yes_no_buttons(uuid) when uuid in 0..0xFF_FF_FF do
